0272e0e60ca244d1d8ea7a3fd1139c69a2d31e93
shaheem-khanzada <shaheem340@gmail.com>
2026-01-01 19:07:35 +0500

    chore: updated port

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const ENV = {
-  PORT: process.env.PORT || '3000',
+  PORT: process.env.PORT || 4000,
   REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
 }
